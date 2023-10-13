10c0c9990fe0363a133683abbc1f58b3a8f36af3
GostivariMattia <mattia.gostivari@alessandrinimainardi.edu.it>
2023-10-13 13:12:39 +0200

    passo 9

--- prima_app/views.py
+++ prima_app/views.py
@@ -13,3 +13,14 @@ def lista(request):
 
 def chi_siamo(request):
     return render(request,"chi_siamo.html")
+
+def variabili(request):
+    context= {
+        'var1': 'prima_variabile',
+        'var2': 'seconda_variabile',
+        'var3': 'terza_variabile'
+    }
+    return render(request,"variabili.html",context)
+
+def index(request):
+    return render(request,"index.html")
