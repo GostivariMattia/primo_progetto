866836767804dde694085107cdf6e96e63b066e2
GostivariMattia <mattia.gostivari@alessandrinimainardi.edu.it>
2023-10-20 10:12:45 +0200

    passo 12

--- seconda_app/views.py
+++ seconda_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def es_if(request):
@@ -17,3 +18,13 @@ def if_else_elif(request):
         'var3' : 100.50,
     }
     return render(request, "if_else_elif.html",context)
+
+def es_for(request):
+    context={
+        'list1': [1,datetime.date(2019,7,16), 'Do not give up'],
+        'list2': [1,datetime.date(2019,7,16), 'Do not give up'],
+    }
+    return render(request, "es_for.html",context)
+
+def index2(request):
+    return render(request, "index2.html")
