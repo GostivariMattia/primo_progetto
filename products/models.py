98c152a47032baeae9184179459681d4d29b807b
GostivariMattia <mattia.gostivari@alessandrinimainardi.edu.it>
2024-04-19 09:40:49 +0200

    passo 24

--- products/models.py
+++ products/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Manufacturer(models.Model):
-    name= models.CharField(max_length=120)
-    location = models.CharField()
+# class Manufacturer(models.Model):
+#     name= models.CharField(max_length=120)
+#     location = models.CharField()
